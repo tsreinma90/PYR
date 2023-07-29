51e2efc6931372528e0669bfa73836954a691e3d
tom.reinman@gmail.com <32964329+tsreinma90@users.noreply.github.com>
2023-07-29 12:03:06 -0400

    removed files no longer needed

--- webpage.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    const menuToggle = document.getElementById('toggle');
-    const showcase = document.getElementById('showcase');
-
-    menuToggle.addEventListener('click', () => {
-        menuToggle.classList.toggle('active');
-        showcase.classList.toggle('active');
-    })
-}
