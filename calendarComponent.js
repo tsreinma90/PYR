08b35e22986f661e3fd9de11f2aa4e2e1c645978
Thomas Reinman <thomasreinman@Thomass-MacBook-Pro.local>
2024-09-29 13:32:10 -0400

    moving helper methods outside app to separate file

--- calendarComponent.js
+++ calendarComponent.js
@@ -1,268 +1,13 @@
-const MONTH_NAMES = [
-  "January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
 jQuery(window).on("load", function () {
   setTimeout(function () {
     setupBarChart(null);
     configureSlider(null);
-    // callOpenAI(null);
   }, 100);
 });
 
-/*async function callOpenAI(userInput) {
-  const response = await fetch('https://devpro-dev-ed.my.salesforce.com/services/apexrest/openai/', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ userInput: userInput })
-  });
-  
-  const data = await response.json();
-  console.log('***', data); // Process the response
-}*/
-
-function toggleSetupWizard(flipToBuilder) {
-  const card = document.querySelector(".relative");
-  card.classList.toggle("flip-card-active");
-  let front = document.querySelector("#front");
-  let back = document.querySelector("#back");
-
-  setTimeout(() => {
-    front.style.display = front.style.display === "none" ? "block" : "none";
-    back.style.display = back.style.display === "none" ? "block" : "none";
-  }, flipToBuilder ? 0 : 450);
-}
-
-function getMonthName(dateString) {
-  const date = new Date(dateString);
-  return MONTH_NAMES[date.getMonth()];
-}
-
 var myChart;
-
-function setupBarChart(workoutEvents) {
-  if (workoutEvents) {
-    let aggregatedData = {};
-
-    workoutEvents.forEach(w => {
-      const month = getMonthName(w.event_date);
-      if (!aggregatedData[month]) {
-        aggregatedData[month] = {
-          "Easy": 0, "Tempo": 0, "Speed": 0, "Long": 0
-        };
-      }
-      aggregatedData[month][w.event_workout] += w.event_distance;
-    });
-
-    const ctx = document.getElementById("myChart").getContext("2d");
-    if (myChart) {
-      myChart.destroy();
-    }
-
-    myChart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: Object.keys(aggregatedData),
-        datasets: [
-          {
-            label: "Easy",
-            backgroundColor: "rgba(167, 243, 208, 0.2)",
-            borderColor: "rgba(167, 243, 208, 1)",
-            borderWidth: 1,
-            data: Object.values(aggregatedData).map(monthData => monthData.Easy)
-          },
-          {
-            label: "Tempo",
-            backgroundColor: "rgba(59, 130, 246, 0.2)",
-            borderColor: "rgba(59, 130, 246, 1)",
-            borderWidth: 1,
-            data: Object.values(aggregatedData).map(monthData => monthData.Tempo)
-          },
-          {
-            label: "Speed",
-            backgroundColor: "rgba(216, 4, 4, 0.2)",
-            borderColor: "rgba(216, 4, 4, 1)",
-            borderWidth: 1,
-            data: Object.values(aggregatedData).map(monthData => monthData.Speed)
-          },
-          {
-            label: "Long",
-            backgroundColor: "rgba(118, 1, 168, 0.2)",
-            borderColor: "rgba(118, 1, 168, 1)",
-            borderWidth: 1,
-            data: Object.values(aggregatedData).map(monthData => monthData.Long)
-          }
-        ]
-      },
-      options: {
-        scales: {
-          xAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }],
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
-  }
-}
-
 let init = false;
 let activeSliderListeners = [];
-
-function configureSlider(details) {
-  var weeklyMileageSlider = document.getElementById("weeklyMileageSlider");
-  var workoutPercentSlider = document.getElementById("workoutPercentSlider");
-
-  if (!details && !init) {
-    init = true;
-    let valuesForSlider = [];
-    const format = {
-      to: function (value) {
-        return valuesForSlider[Math.round(value)];
-      },
-      from: function (value) {
-        return valuesForSlider.indexOf(Number(value));
-      }
-    };
-
-    for (let i = 0; i < 100; i++) {
-      valuesForSlider.push(i);
-    }
-
-    noUiSlider.create(weeklyMileageSlider, {
-      start: [20],
-      connect: [true, true],
-      tooltips: true,
-      format: format,
-      range: {
-        min: [1],
-        max: [100]
-      }
-    });
-
-    noUiSlider.create(workoutPercentSlider, {
-      start: [100],
-      connect: [true, true],
-      range: { min: [0], max: [100] },
-      disabled: true
-    });
-
-    let connect = workoutPercentSlider.querySelectorAll(".noUi-connect");
-    connect[0].classList.add("c-1-color");
-  } else if (details) {
-    workoutPercentSlider.noUiSlider.off('update', activeSliderListeners[0]);
-    workoutPercentSlider.noUiSlider.destroy();
-
-    noUiSlider.create(workoutPercentSlider, details);
-    details.disabled === true
-      ? workoutPercentSlider.noUiSlider.disable()
-      : workoutPercentSlider.noUiSlider.enable();
-
-    let connect = workoutPercentSlider.querySelectorAll(".noUi-connect");
-    details.classes.forEach((className, i) => {
-      connect[i].classList.add(className);
-    });
-
-    const sliderUpdate = workoutPercentSlider.noUiSlider.on('update', function (values, handle) {
-      updateSliderLegend(details, values, handle);
-    });
-
-    activeSliderListeners.push(sliderUpdate);
-  }
-}
-
-function updateSliderLegend(details, values, handle) {
-  const selectedWorkouts = details["classes"];
-  const numSelections = values.length;
-  const oneHundred =
-    details["start"].length === 1 && details["start"][0] === 100 && selectedWorkouts.length > 0;
-
-  let legendMap = new Map();
-  legendMap.set('c-1-color', document.getElementById('easy'));
-  legendMap.set('c-2-color', document.getElementById('tempo'));
-  legendMap.set('c-3-color', document.getElementById('speed'));
-  legendMap.set('c-4-color', document.getElementById('long'));
-
-  let first = legendMap.get(selectedWorkouts[0]);
-  let second = legendMap.get(selectedWorkouts[1]);
-  let third = legendMap.get(selectedWorkouts[2]);
-  let fourth = legendMap.get(selectedWorkouts[3]);
-    
-  for (const [key] of legendMap) {
-    if (!selectedWorkouts.includes(key)) {
-      legendMap.get(key).innerHTML = 0 + '%';
-    }
-  }
-
-  if (oneHundred) {
-    legendMap.get(selectedWorkouts[0]).innerHTML = 100 + '%';
-  } else if (selectedWorkouts.length) {
-    switch (numSelections) {  
-      case 1:
-        first.innerHTML = Math.round(values[handle]) + '%';
-        second.innerHTML = Math.round(100 - values[handle]) + '%';
-        return null;
-
-      case 2:
-        if (handle === 0) {
-          first.innerHTML = Math.round(values[handle]) + '%';
-          second.innerHTML = Math.round(values[handle+1] - values[handle]) + '%';
-          return null;
-        } else if (handle === 1) {
-          third.innerHTML = Math.round(100 - values[handle]) + '%';
-          second.innerHTML = Math.round(100 - (Math.round(values[0]) + Math.round(100 - values[handle]))) + '%';
-          return null;
-        }
-
-        case 3:
-          if (handle === 0) {
-            first.innerHTML = Math.round(values[handle]) + '%';
-            second.innerHTML = Math.round(values[handle+1] - values[handle]) + '%';
-            return null;
-
-          } else if (handle === 1) {
-            second.innerHTML = Math.round(values[1] - values[0]) + '%';
-            third.innerHTML = Math.round(values[2] - values[1]) + '%';
-            return null;
-
-          } else if (handle === 2) {
-            third.innerHTML = Math.round(values[2] - values[1]) + '%';
-            fourth.innerHTML = Math.round(100 - (Math.round(values[0]) + Math.round(values[1] - values[0]) + Math.round(values[2] - values[1]))) + '%';
-          }
-
-      default:
-        return null;
-    }
-  }
-}
-
-function calculateDefaults(selectedWorkouts) {
-  const conditions = {
-    Easy: false,
-    Tempo: false,
-    Speed: false,
-    Long: false
-  };
-
-  for (const key in selectedWorkouts) {
-    if (selectedWorkouts[key] > 0 && key in conditions) {
-      conditions[key] = true;
-    }
-  }
-
-  const trueConditions = Object.keys(conditions).filter(key => conditions[key]);
-  return window.slider.getOptions(trueConditions.join("+"));
-}
 
 function app() {
   return {
@@ -305,7 +50,7 @@ function app() {
 
     numOfWeeksInTraining: 0,
 
-    handleWorkoutSelection(addSelection, selection) {
+    /*handleWorkoutSelection(addSelection, selection) {
       if (addSelection) {
         this.uniqueWorkoutTracker[selection] += 1;
         const sliderPresets = calculateDefaults(this.uniqueWorkoutTracker);
@@ -314,7 +59,7 @@ function app() {
         if (this.uniqueWorkoutTracker[selection] > 0)
           this.uniqueWorkoutTracker[selection] -= 1;
       }
-    },
+    },*/
 
     workoutColorPairs: [
       { value: "green", label: "Easy" },
@@ -432,6 +177,10 @@ function app() {
       }, duration);
     },
 
+    load() {
+      console.log('***', window.popcorn);
+    },
+
     initDate() {
       let today = new Date();
       this.month = today.getMonth();
@@ -444,32 +193,6 @@ function app() {
       const d = new Date(this.year, this.month, date);
       return today.toDateString() === d.toDateString();
     },
-
-    /*showEventModal(date, dateIndex, eventTitle) {
-      let dIndx = this.fetchCalendarEventByDateIndex(dateIndex + 1);
-      if (dIndx != -1) {
-        let selectedWorkout = !eventTitle
-          ? this.workouts[dIndx]
-          : this.workouts[dIndx].event_title == eventTitle
-            ? this.workouts[dIndx]
-            : this.workouts[dIndx + 1];
-        this.event_title = selectedWorkout.event_title;
-        this.event_workout = selectedWorkout.event_workout;
-        this.event_distance = selectedWorkout.event_distance;
-        this.event_notes = selectedWorkout.event_notes;
-        this.event_theme = selectedWorkout.event_theme;
-      } else {
-        this.event_title = "";
-        this.event_workout = "Easy";
-        this.event_distance = 1;
-        this.event_notes = "";
-        this.event_theme = "green";
-      }
-
-      this.setSelectedOption("workoutSelect", this.event_workout);
-      this.openEventModal = true;
-      this.event_date = new Date(this.year, this.month, date).toDateString();
-    },*/
 
     showEventModal(date, dateIndex, eventTitle) {
       // Fetch the index of the calendar event by date
@@ -627,35 +350,4 @@ function app() {
       downloadHelper.downloadCalendarFile(this.workouts);
     }
   };
-}
-
-function resizeListener(elementId) {
-  var $element = $("#" + elementId);
-  var $children = $element.children();
-  var initial = false;
-  $(window)
-    .on("resize", function () {
-      if (initial) {
-        const width = $(window).width() / $element.width();
-        const height = $(window).height() / $element.height();
-        const scale = `scale(${Math.min(width, height)})`;
-        $element.css({
-          "-webkit-transform": scale,
-          "-moz-transform": scale,
-          "-ms-transform": scale,
-          "-o-transform": scale,
-          transform: scale
-        });
-        $children.css({
-          "-webkit-transform": scale,
-          "-moz-transform": scale,
-          "-ms-transform": scale,
-          "-o-transform": scale,
-          transform: scale
-        });
-      } else {
-        initial = true;
-      }
-    })
-    .trigger("resize");
 }
